e579c3c7f45c106f7efe0c4ca114a2354857fa53
farhanzip <farhanaji317@gmail.com>
2023-01-20 13:52:22 +0700

    first commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,27 +1,38 @@
 import React from 'react'
 import '../assets/landingPage.css'
-import { Button, Container, Input } from 'react-bootstrap'
+import { Button, Container, Card } from 'react-bootstrap'
 
 const Hero = () => {
   return (
-    <Container fluid className='landing-container'>
-      <div className='hero-wrapper'>
+    <Container className='landing-container'>
+      <div className='hero-wrapper text-wrapper'>
         
         <h1 className='hero-title'>
           Jadilah Bagian dari
           <br /> Komunitas Tech Terbesar
           <br /> di Indonesia
         </h1>
-        <h5>
+        <h5 className='hero-p'>
           Ikutan diskusi forum tanya jawab, tulis blog dan
           <br />Membangun portofolio semua di Kotakode
         </h5>
-        <div className='input-wrapper'>
-          <input />
-          <Button>Daftar</Button>
+        <div className='input-wrapper justify-content-center'>
+          <input type="email" className='input-box' placeholder='Masukkan Email Kamu'/>
+          <Button className='input-button '>Daftar</Button>
         </div>
         <div className='total-wrapper'>
-          <div className='total-box'></div>
+          <Card className=" text-center total-box">
+            <Card.Title><strong>29564</strong></Card.Title>
+            User Bergabung
+          </Card>
+          <Card className=" text-center total-box">
+            <Card.Title><strong>1078</strong></Card.Title>
+            Blog Ditulis
+          </Card>
+          <Card className=" text-center total-box">
+            <Card.Title><strong>19779</strong></Card.Title>
+            Total Pertanyaan & Jawaban
+          </Card>
         </div>
     </div>
     </Container>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,12 +9,12 @@ const Navigation = () => {
         <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="text-center">
             <Nav.Link className="nav-link" href="/home"> Home</Nav.Link>
             <Nav.Link className="nav-link" href="/about"> About</Nav.Link>
           </Nav>
           <Nav className='gap-2'>
-            <Button className='' variant='success'>Register</Button>
+            <Button className='' variant='outline-success'>Register</Button>
             <Button className=''>Sign In</Button>
           </Nav>
         </Navbar.Collapse>
