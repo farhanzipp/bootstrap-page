f87f3c1c7afc240c9d7db9718accce595b33ff35
farhanzip <farhanaji317@gmail.com>
2023-01-24 12:37:43 +0700

    why component tablet and laptop viewport

--- src/components/Why.js
+++ src/components/Why.js
@@ -13,32 +13,32 @@ const Why = () => {
             <Card.Body className='text-center why-content-item'>
               <Container className='why-header'>
                 <h1 className='hero-title'>Mengapa perlu menggunakan <br />Kotakode?</h1>
-                <p>Ibaratkan sebuah kota dengan penduduk para programmer, Kotakode memberikan fasilitas guna mensejahterakan kehidupan penduduknya.</p>
+                <p className='why-description'>Ibaratkan sebuah kota dengan penduduk para programmer, Kotakode memberikan fasilitas guna mensejahterakan kehidupan penduduknya.</p>
               </Container>
               <Container className='why-contents'>
                   <Container className='why-left'>
                     <Card className='why-card'>
                         <img src={cat1} alt='cat1' className='why-img' />
-                        <Container className='why-card-text'>
+                        <Card.Body className='why-card-text'>
                         <h4><b>Jawaban cepat, tepat & gratis</b></h4>
                         <p>Dapatkan jawaban dalam hitungan menit dari ribuan programmer lainnya.</p>
-                        </Container>
+                        </Card.Body>
                     </Card>
-                    <Card className='why-card mt-5'>
-                        <img src={cat2} alt='cat1'className='why-img'/>
-                        <Container className='why-card-text'>
+                    <Card className='why-card'>
+                        <img src={cat2} alt='cat2'className='why-img'/>
+                        <Card.Body className='why-card-text'>
                         <h4><b>Konsep Gamifikasi</b></h4>
                         <p>Dapatkan badge menarik yang akan meningkatkan personal branding kamu sebagai seorang programmer.</p>
-                        </Container>
+                        </Card.Body>
                     </Card>
                   </Container>
                   <Container className='why-right'>
-                    <Card className='why-card'>
-                        <img src={cat3} alt='cat1' className='why-img' />
-                        <Container className='why-card-text'>
+                    <Card className='why-card porto'>
+                        <img src={cat3} alt='cat3' className='why-img cat3' />
+                        <Card.Body className='why-card-text text3'>
                         <h1 className='hero-title'>Bangun Online Portofolio</h1>
                         <p>Portofolio ibaratkan sebuah aset yang berharga bagi setiap orang. Semakin kamu aktif di dalam forum, semakin membuktikan bahwa kamu adalah programmer yang berkualitas.</p>
-                        </Container>
+                        </Card.Body>
                     </Card>
                   </Container>
               </Container>
